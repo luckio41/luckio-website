2ef8dec39d9a5de00cf81539126222a6fed4c489
Desarrollo BioBioChile <emunoz.ccp@gmail.com>
2017-06-27 09:39:45 -0400

    Test image with http

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>{{ msg }}</h1>
+    <img src="http://media.pagina7.cl/2017/06/000_PY86L-700x457.jpg" />
   </div>
 </template>
 
@@ -17,6 +18,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.about {
+  text-align: center;
+}
 h1, h2 {
   font-weight: normal;
   color: red;
