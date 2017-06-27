1ab223e58d8c0323584c8f0a132257b94da29fca
Desarrollo BioBioChile <emunoz.ccp@gmail.com>
2017-06-27 15:01:17 -0400

    Remove https content.

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="about">
     <h1>{{ msg }}</h1>
-    <img src="http://media.pagina7.cl/2017/06/000_PY86L-700x457.jpg" />
-    <audio class="wp-audio-shortcode" id="audio-3282943-1" preload="none" style="width: 100%;" controls="controls"><source type="audio/mpeg" src="http://media.biobiochile.cl/wp-content/uploads/2017/06/963-cuna-aborto-objecion-conciencia-maturana.mp3?_=1"><a href="http://media.biobiochile.cl/wp-content/uploads/2017/06/963-cuna-aborto-objecion-conciencia-maturana.mp3">http://media.biobiochile.cl/wp-content/uploads/2017/06/963-cuna-aborto-objecion-conciencia-maturana.mp3</a></audio>
   </div>
 </template>
 
